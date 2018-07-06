31e77120d0a0dec5bba931a04f32ae29f81bccb2
zhangcj7 <2393658870@qq.com>
2018-07-06 10:22:26 +0800

    zhangcj7

---
+++ files/框架--2/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[_(W,X,Y,j,Z,ba,q,bb,bc,bb,bd,be,v,_(bf,_(bg,bh,bi,bj),bk,_(bl,bh,bm,bn)),S,_(),bo,_(),bp,_(bq,n,br,be))])),bs,_(),bt,_(bu,_(bv,bw)));}; 
+var b="url",c="框架--2.html",d="generationDate",e=new Date(1530759456888.61),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="5eab9f2fbde84d2b909918fa4cf4d21a",q="type",r="Axure:Page",s="name",t="框架--2",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="id",X="a02596058d9a42cb94e7b6f3d9c0c266",Y="label",Z="friendlyType",ba="内联框架",bb="inlineFrame",bc="styleType",bd="visible",be=true,bf="size",bg="width",bh=360,bi="height",bj=96,bk="location",bl="x",bm="y",bn=65,bo="imageOverrides",bp="target",bq="targetType",br="includeVariables",bs="masters",bt="objectPaths",bu="a02596058d9a42cb94e7b6f3d9c0c266",bv="scriptId",bw="u1001";
+return _creator();
+})());
